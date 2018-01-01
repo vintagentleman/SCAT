04b3af4c0cc9ee0290ce07530c54583636ec5dad
Konstantin Sipunin <79818514079@yandex.ru>
2018-01-01 13:48:24 +0300

    Optimise lemmatiser & localise tokens by line number

--- lemmatiser.py
+++ lemmatiser.py
@@ -1,44 +1,49 @@
 import os
+import csv
 import glob
 from txt_to_xml import Token
 
 
+def process():
+    # Все файлы с разметкой в папке со входными данными
+    files = glob.glob('*.txt')
+
+    for file in files:
+        f = open(file, mode='r', encoding='utf-8')
+
+        for i, line in enumerate(f):
+            # Непечатные символы где-нибудь да проскакивают - надо избавляться
+            items = [item.strip() for item in line.rstrip('\n').split(sep='\t')]
+
+            if len(items) == 7:
+                yield Token(items[0], '%s.%d' % (file[:-4], i + 1), [items[i] for i in range(1, 7)])
+            else:
+                print('Warning: corrupt data in file %s, line %d.' % (file, i + 1))
+                continue
+
+        f.close()
+
+
 if __name__ == '__main__':
-    out = open('output.txt', mode='w', encoding='utf-8')
-    err = open('errors.txt', mode='w', encoding='utf-8')
+    out = open('output.csv', mode='w', encoding='utf-8', newline='')
+    err = open('errors.csv', mode='w', encoding='utf-8', newline='')
 
     try:
         os.chdir(os.getcwd() + '\\data')
-        print('Please wait. Python is processing your data...')
-
-        # Все файлы с разметкой в папке со входными данными
-        files = glob.glob('*.txt')
-
-        for file in files:
-
-            f = open(file, mode='r', encoding='utf-8')
-
-            for i, line in enumerate(f):
-                # Непечатные символы где-нибудь да проскакивают - надо избавляться
-                items = [item.strip() for item in line.rstrip('\n').split(sep='\t')]
-
-                if len(items) == 7:
-                    t = Token(items[0], file[:-4], [items[i] for i in range(1, 7)])
-                else:
-                    print('Warning: corrupt data in file %s, line %d.' % (file, i + 1))
-                    continue
-
-                # Вот и всё колдовство
-                if hasattr(t, 'lemma'):
-                    out.write('\t'.join([t.token_id, t.src, t.lemma] + t.ana) + '\n')
-
-                    if t.lemma == 'NONE':
-                        err.write('\t'.join([t.token_id, t.src, t.lemma] + t.ana) + '\n')
-
-            f.close()
-
     except FileNotFoundError:
         print('Error: source data directory missing.')
+    else:
+        print('Please wait. Python is processing your data...')
+        out_writer = csv.writer(out, delimiter='\t')
+        err_writer = csv.writer(err, delimiter='\t')
+
+        for t in process():
+            if hasattr(t, 'lemma'):
+                row = [t.token_id, t.src, t.lemma] + t.ana
+                out_writer.writerow(row)
+
+                if t.lemma == 'NONE':
+                    err_writer.writerow(row)
 
     finally:
         err.close()
