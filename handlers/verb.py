dcde35e8d733b2b18a11c37ce9229a6b7feeb4a1
Konstantin Sipunin <79818514079@yandex.ru>
2018-03-11 14:24:17 +0300

    Amend verb lemmatisation

--- handlers/verb.py
+++ handlers/verb.py
@@ -5,20 +5,19 @@ from handlers import Verb
 
 
 def cls_cons_modif(s):
-    # 7-й класс (основы на гласный)
-    for regex in lib.cls_7_vow:
-        if re.match('(.*)%s$' % regex, s):
-            return s, 'СТИ'
+    # VI.2.а и VII.1 классы: основы на гласный
+    if re.search('(БЛЮ|БРЕ|ВЕ|КЛА|КЛЯ|КРА|МЕ|ПА|ПЛЕ|СЕ|ЧЕ)$', s):
+        return s, 'СТИ'
 
-    # 7-й класс (основы на согласный)
-    for regex in lib.cls_7_cons:
+    # VI.2.а и VII.1 классы: основы на согласный
+    for regex in lib.cnj_1_sti:
         mo = re.match('(.*)%s$' % regex, s)
         if mo:
-            return re.sub('(.*)%s$' % regex, mo.group(1) + lib.cls_7_cons[regex], s), 'ТИ'
+            return re.sub('(.*)%s$' % regex, mo.group(1) + lib.cnj_1_sti[regex], s), 'ТИ'
 
-    # 8-й класс
-    for regex in lib.cls_8:
-        if re.match('(.*)%s$' % regex, s):
+    # VI.1 класс
+    for regex in lib.cnj_1_tschi:
+        if re.search('%s$' % regex, s):
             s = s[:-1]
 
             # Чередование с нулём
@@ -29,9 +28,11 @@ def cls_cons_modif(s):
 
             return s, 'ЩИ'
 
-    # 9-й класс
-    if re.match('(.*)[МПТ][+Е]Р$', s):
-        return s, 'ЕТИ'
+    # VI.2.б и VI.2.в классы
+    if re.search('[МПТ][+Е]Р$', s):
+        return s + 'Е', 'ТИ'
+    elif re.search('ШИБ$', s):
+        return s + 'И', 'ТИ'
 
     return s, ''
 
@@ -39,13 +40,11 @@ def cls_cons_modif(s):
 def part_el(gr):
     # Стемминг
     s_old = tools.find_stem(gr.form, (gr.gen, gr.num), lib.part_el_infl)
-    s_new = s_old
 
-    if s_new == 'NONE':
+    if s_old == 'NONE':
         return ('', 'NONE'), ''
-
-    if s_new.endswith('Л'):
-        s_new = s_new[:-1]
+    else:
+        s_new = s_old
 
     # Основы-исключения
     for regex in lib.part_el_spec:
@@ -60,7 +59,7 @@ def part_el(gr):
     if infl:
         return (s_old, s_modif), infl
 
-    # 3*-й класс
+    # 4 класс
     if s_new[-1] in lib.cons or s_new in ('ВЯ', 'СТЫ'):
         s_new += 'НУ'
 
@@ -70,10 +69,11 @@ def part_el(gr):
 def aor_simp(gr):
     # Стемминг
     s_old = tools.find_stem(gr.form, (gr.pers, gr.num), lib.aor_simp_infl)
-    s_new = s_old
 
-    if s_new == 'NONE':
+    if s_old == 'NONE':
         return ('', 'NONE'), ''
+    else:
+        s_new = s_old
 
     # Основы-исключения (настоящего времени)
     if s_new.endswith(('ДАД', 'ЖИВ', 'ИД', 'ЫД')):
@@ -87,7 +87,7 @@ def aor_simp(gr):
     if infl:
         return (s_old, s_modif), infl
 
-    # 3*-й класс
+    # 4 класс
     if s_new[-1] in lib.cons or s_new in ('ВЯ', 'СТЫ'):
         s_new += 'НУ'
 
@@ -109,32 +109,27 @@ def aor_sigm(gr):
     if gr.tense == 'аор нов' and s_old.endswith('О'):
         s_old = s_old[:-1]
 
-    s_new = s_old
-
-    if s_new == 'NONE':
+    if s_old == 'NONE':
         return ('', 'NONE'), ''
     elif gr.tense == 'аор гл':
-        return (s_old, s_new), 'ТИ'
+        return (s_old, s_old), 'ТИ'
+    else:
+        s_new = s_old
 
     # Основы-исключения (настоящего времени)
     if s_new.endswith(('ДАД', 'ЖИВ', 'ИД', 'ЫД')):
         s_new = s_new[:-1]
 
-    if gr.tense == 'аор сигм':
-        # Удлинение корневого гласного
-        if s_new[-1] == '+':
-            s_new = s_new[:-1] + 'Е'
-
-        # На рассматриваемом материале лексема одна-единственная
-        if s_new == 'РЕ':
-            return (s_old, s_new), 'ЩИ'
+    # Удлинение корневого гласного
+    if gr.tense == 'аор сигм' and s_new == 'Р+':
+        return (s_old, 'РЕ'), 'ЩИ'
 
     # Проблемные классы
     s_modif, infl = cls_cons_modif(s_new)
     if infl:
         return (s_old, s_modif), infl
 
-    # 3*-й класс
+    # 4 класс
     if s_new[-1] in lib.cons or s_new in ('ВЯ', 'СТЫ'):
         s_new += 'НУ'
 
@@ -146,7 +141,8 @@ def main(token):
     stem, fl = ('', ''), ''
 
     if gr.mood == 'изъяв':
-        # Простые времена
+        # --- Простые времена --- #
+
         if gr.tense == 'прош':
             stem, fl = part_el(gr)
         elif gr.tense == 'аор пр':
@@ -155,7 +151,7 @@ def main(token):
             stem, fl = aor_sigm(gr)
 
         elif gr.tense == 'а/имп':
-            # Тут лексема по факту одна-единственная
+            # Тут лексема одна-единственная
             if gr.pers in ('2', '3') and gr.num == 'ед':
                 s_old = gr.form
             else:
@@ -164,25 +160,29 @@ def main(token):
             if s_old != 'NONE':
                 stem, fl = (s_old, 'БЫ'), 'ТИ'
 
-        # Сложные времена
+        # --- Сложные времена --- #
+
         elif re.match('перф|плюскв|буд ?[12]', gr.tense):
+
             if gr.role.endswith('св'):
                 if gr.tense in lib.ana_tenses:
                     return ('', lib.ana_tenses[gr.tense]), ''
                 else:
                     return ('', 'NONE'), ''
+
             elif gr.role == 'инф':
                 return ('', gr.form), ''
             elif gr.role.startswith('пр'):
                 stem, fl = part_el(gr)
 
     elif gr.mood == 'сосл':
+
         if gr.role == 'св':
             return ('', 'AUX-SBJ'), ''
         elif gr.role.startswith('пр'):
             stem, fl = part_el(gr)
 
-    # Убрать, когда закончим со всеми временами
+    # В конце всей эпопеи убрать первое условие
     if stem[1] not in ('', 'NONE') and gr.refl:
         fl += 'СЯ'
 
