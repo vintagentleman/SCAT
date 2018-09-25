9d804fdbc70df251a8e24a1ccb4fe52d1f48dc19
Konstantin Sipunin <k.sipunin@just-ai.com>
2018-09-25 23:02:47 +0300

    Split workload per each student and exclude fully annotated forms from the count

--- annotator.py
+++ annotator.py
@@ -1,59 +1,62 @@
 import os
-import sys
 import csv
 import xlsxwriter
 from collections import defaultdict
 import obj
 
 
-def main(fn, offset=0, limit=sys.maxsize):
+def main(fn, st_total, limit, offset=0):
     os.chdir(root + '\\txt')
 
-    fo = open(fn + '.csv', mode='r', encoding='utf-8')
+    fo = open(fn + '.csv', mode='r', encoding='utf-8', newline='')
     reader = csv.reader(fo, delimiter='\t')
+    all(next(reader) for _ in range(offset))
+
     book = xlsxwriter.Workbook(fn + '.xlsx')
     # ambi = book.add_format({'bg_color': 'yellow'})
     ok = book.add_format({'bg_color': 'lime'})
+    st_count = 0
 
-    sheet = book.add_worksheet()
-    sheet.set_column('A:A', 40)
+    while st_count < st_total:
+        sheet = book.add_worksheet()
+        sheet.set_column('A:A', 40)
+        st_limit = limit
 
-    for i, row in enumerate(reader):
-        if i < offset:
-            continue
+        for i, row in enumerate(reader):
+            if len(row) == 1:
+                sheet.write(i, 0, row[0])
+                form = obj.Word(obj.Token(row[0]).word, fn, i).reg
+                if form and form in db:
+                    # Если разбор один, то помечаем его и не учитываем при подсчёте
+                    if len(db[form]) == 1:
+                        sheet.write(i, 0, row[0], ok)
+                        sheet.write_row(i, 1, db[form][0])
+                        st_limit += 1
+                    # Если нет, то выделяем неоднозначные позиции
+                    else:
+                        msd = zip(*db[form])
 
-        if len(row) == 1:
-            sheet.write(i, 0, row[0])
-            form = obj.Word(obj.Token(row[0]).word, fn, i).reg
-            if form and form in db:
-                # Если разбор один, то помечаем его как единственный
-                if len(db[form]) == 1:
-                    sheet.write(i, 0, row[0], ok)
-                    sheet.write_row(i, 1, db[form][0])
-                # Если нет, то выделяем неоднозначные позиции
-                else:
-                    msd = zip(*db[form])
+                        for j, col in enumerate(msd, start=1):
+                            # Если конфликтов нет, то просто записываем
+                            if col.count(col[0]) == len(col):
+                                sheet.write(i, j, col[0])
+                            # Конфликты выделяем и отображаем все опции
+                            # else:
+                            #     sheet.write_blank(i, j, None, ambi)
+                            #     sheet.data_validation(i, j, i, j, {
+                            #         'validate': 'list',
+                            #         'source': list(dict.fromkeys(col)),
+                            #         'show_error': False
+                            #     })
 
-                    for j, col in enumerate(msd, start=1):
-                        # Если конфликтов нет, то просто записываем
-                        if col.count(col[0]) == len(col):
-                            sheet.write(i, j, col[0])
-                        # Конфликты выделяем и отображаем все опции
-                        # else:
-                        #     sheet.write_blank(i, j, None, ambi)
-                        #     sheet.data_validation(i, j, i, j, {
-                        #         'validate': 'list',
-                        #         'source': list(dict.fromkeys(col)),
-                        #         'show_error': False
-                        #     })
+            # Если колонок больше одной, то это цифирь
+            else:
+                sheet.write(i, 0, row[0], ok)
+                sheet.write(i, 1, row[1])
 
-        # Если колонок больше одной, то это цифирь
-        else:
-            sheet.write(i, 0, row[0], ok)
-            sheet.write(i, 1, row[1])
-
-        if i == limit:
-            break
+            if i == st_limit:
+                st_count += 1
+                break
 
     fo.close()
     book.close()
@@ -70,4 +73,4 @@ if __name__ == '__main__':
             if parse not in db[t.reg]:
                 db[t.reg].append(parse)
 
-    main('KrnKml')
+    main('KrnKml', 10, 250)
